bb86cc23eeeb118c97ba0dd76b8d9171f981a8c6
bmanjuu <bettyofung@gmail.com>
2016-06-13 17:13:07 -0400

    all methods complete except for locationsNearLatitude:longitude:margin

--- locationTrivia-Objects/FISAppDelegate.m
+++ locationTrivia-Objects/FISAppDelegate.m
@@ -9,4 +9,47 @@
     return YES;
 }
 
+-(NSArray *)allLocationNames{
+    
+    NSMutableArray *locationNames = [[NSMutableArray alloc] init];
+    
+    for(FISLocation *location in self.locations){
+        [locationNames addObject:location.name];
+    }
+    
+    return locationNames;
+}
+
+
+
+-(FISLocation *)locationNamed:(NSString *)name{
+    
+    for(FISLocation *location in self.locations){
+        if([location.name isEqualToString:name]){
+            return location;
+        }
+    }
+    
+    return nil;
+}
+
+
+
+-(NSArray *)locationsNearLatitude:(CGFloat)latitude longitude:(CGFloat)longitude margin:(CGFloat)margin{
+    
+    NSMutableArray *locationsWithinMargin = [[NSMutableArray alloc] init];
+    
+    NSRange marginForRange = NSMakeRange(0, 25);
+    
+    for(FISLocation *location in self.locations){
+        BOOL latitudeInRange = NSLocationInRange(location.latitude, marginForRange);
+        BOOL longitudeInRange = NSLocationInRange(location.longitude, marginForRange);
+        if(latitudeInRange && longitudeInRange){
+            [locationsWithinMargin addObject:location];
+        }
+    }
+    
+    return locationsWithinMargin;
+}
+
 @end
---
+++ locationTrivia-Objects/FISLocation.m
@@ -0,0 +1,83 @@
+//
+//  FISLocation.m
+//  locationTrivia-Objects
+//
+//  Created by Betty Fung on 6/13/16.
+//  Copyright © 2016 Joe Burgess. All rights reserved.
+//
+
+#import "FISLocation.h"
+#import "FISTrivium.h"
+
+@implementation FISLocation
+
+-(instancetype)init{
+    self = [self initWithName:@"" latitude:0.0 longitude:0.0];
+    return self;
+}
+
+
+-(instancetype)initWithName:(NSString *)name latitude:(CGFloat)latitude longitude:(CGFloat)longitude {
+    self = [super init];
+    
+    if (self){
+        _name = name;
+        _latitude = latitude;
+        _longitude = longitude;
+        
+        _trivia = [[NSMutableArray alloc] init];
+    }
+    
+    return self;
+}
+
+
+-(NSString *)stringByTruncatingNameToLength:(NSUInteger)length{
+    
+    NSString *truncatedName = @"";
+    
+    if (length > [self.name length]) {
+        truncatedName = [truncatedName stringByAppendingString:self.name];
+    }
+    else {
+        truncatedName = [self.name substringToIndex:length];
+    }
+    
+    return truncatedName;
+}
+
+
+
+-(BOOL)hasValidData{
+    
+    if([self.name length] == 0){
+        return NO;
+    }
+    else if(self.latitude < -90.0 || self.latitude > 90.0){
+        return NO;
+    }
+    else if(self.longitude <= -180.0 || self.longitude > 180.00){
+        return NO;
+    }
+    
+    // can essentially nest all these conditions into one big if()-statement but this set up makes it easier to read
+    
+    return YES;
+}
+
+
+
+-(FISTrivium *)triviumWithMostLikes{
+    
+    if([self.trivia count] > 0){
+        NSSortDescriptor *getMostLikes = [[NSSortDescriptor alloc] initWithKey:@"likes" ascending:NO];
+        [self.trivia sortUsingDescriptors:@[getMostLikes]];
+        //get everything in descending order so the object with most likes is first
+        return self.trivia[0];
+    }
+    
+    return nil;
+}
+
+
+@end
